fe00fa1a88b34180c8324b7968b3c164914cc008
Sean Thorpe <sean.thorpe@west-mec.org>
2015-10-22 14:19:30 -0700

    no message

--- picscript.js
+++ picscript.js
@@ -1,3 +1,5 @@
-/**
- * Created by session1 on 10/20/15.
- */
+$(document).ready(function() {
+    $('#cryImg').click(function () {
+        $('#cryImg').attr('src', 'http://wp.production.patheos.com/blogs/lovejoyfeminism/files/2013/04/tantrum.jpg');
+    });
+});
